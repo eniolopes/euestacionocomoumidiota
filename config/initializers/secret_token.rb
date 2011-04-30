1d0a1a0125a194e6dcbeb090a95e9e482302ff76
Enio Lopes <eniolopes@gmail.com>
2011-04-30 14:52:21 -0300

    Subindo o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Euestacionocomoidiota::Application.config.secret_token = '1cfe3c376c746df0a8bf7df0719cff9f2cf2074f5611b2d9d2e57f019b271911cd4958260a3a75b8f7554f9bbbb63ad3d01d9b5daf7e2dc8408fd39bb629121c'
